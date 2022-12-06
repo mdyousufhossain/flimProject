16a39c938b902b6acc1d87e7feeb854fb93bbe35
mdyousufhossain <yousafhossain3@gmail.com>
2022-12-07 02:39:30 +0600

    wokring on navbar

--- flimApp/src/components/NavBar/NavBar.jsx
+++ flimApp/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,67 @@
 import React from 'react';
-import {IconButton, Toolbar, Drawer, Button, Avatar, AppBar } from '@mui/material';
+import { IconButton, Toolbar, Drawer, Button, Avatar, AppBar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 import useStyles from './style';
 
 function NavBar() {
   const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = false;
   return (
     <AppBar position="fixed">
       <Toolbar className={classes.toolbar}>
-        bara
+        {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        )}
+        <IconButton
+          color="inherit"
+          sx={{ ml: 1 }}
+          onClick={() => {}}
+        >
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        {isMobile && 'Search...'}
+        <div>
+          {
+            !isAuthenticated ? (
+              <Button
+                color="inherit"
+                onClick={() => {}}
+              >
+                Login &nbsp; <AccountCircle />
+              </Button>
+
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/123"
+                className={classess.linkButton}
+              >
+                {isMobile && <> myMovies &nbsp ; </> }
+                <Avatar
+                  style={{
+                    width: 30, height: 30,
+                  }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                />
+              </Button>
+
+            )
+          }
+        </div>
       </Toolbar>
     </AppBar>
   );
--- flimApp/src/main.jsx
+++ flimApp/src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
